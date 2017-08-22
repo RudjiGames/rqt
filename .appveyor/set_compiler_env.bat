67149b7d7e460f83ea2d670d48d055e3a1825b41
Milos Tosic <milostosic77@gmail.com>
2017-08-22 15:09:55 +0800

    Debug AppVeyor

--- .appveyor/set_compiler_env.bat
+++ .appveyor/set_compiler_env.bat
@@ -11,39 +11,19 @@ set arch=x86
 
 if "%platform%" EQU "x64" ( set arch=x86_amd64 )
 
-if "%Configuration%"=="2015" (
+if "%TOOLSET%"=="vs2015" (
 	set SET_VS_ENV="C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
 )
 
-if "%Configuration%"=="2013" (
+if "%TOOLSET%"=="vs2013" (
 	set SET_VS_ENV="C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"
 )
 
-if "%Configuration%"=="2012" (
-	set SET_VS_ENV="C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat"
-)
-
-if "%Configuration%"=="2010" (
-	set SET_VS_ENV="C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
-)
-
-if "%Configuration%"=="2008" (
-	set SET_VS_ENV="C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
-)
 @echo "1"
 
 :: Visual Studio detected
-endlocal
+endlocal & call %SET_VS_ENV% %arch%
 
-if "%platform%" EQU "x86" (
-	call %SET_VS_ENV% x86
-)
-
-if "%platform%" EQU "x64" (
-	call %SET_VS_ENV% x64
-)
-
-@rem & call %SET_VS_ENV% %arch%
 @echo "2"
 goto :eof
 
